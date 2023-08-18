63d20dc47d756618361a59ad5ea24a022b938a96
kkopitehong <kkopite.hong@smartcorelink.com>
2023-08-18 18:18:14 +0800

    feat: generate javascript and type

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,14 @@
+import fs from 'node:fs'
 import { describe, expect, it } from 'vitest'
+import { codegen } from '../src/core'
 
-describe('should', () => {
-  it('exported', () => {
-    expect(1).toEqual(1)
+describe('codegen', () => {
+  it('should return code', () => {
+    const swagger = JSON.parse(fs.readFileSync('./example/spec.json', 'utf-8'))
+    const result = codegen({
+      swagger,
+    })
+    expect(result.code).toMatchSnapshot()
+    expect(result.type).toMatchSnapshot()
   })
 })
