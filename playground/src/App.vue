d23bb8d35e66b461012dbd640c4875ae56f84c58
kkopitehong <kkopite.hong@smartcorelink.com>
2023-09-15 17:21:29 +0800

    feat: 实现输出代码功能

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,6 +1,30 @@
+<script lang="ts" setup>
+import { processOpenAPI } from '@clscg/core'
+
+const code = ref('')
+
+const result = computed(() => {
+  try {
+    return processOpenAPI(JSON.parse(code.value))
+  }
+  catch (error) {
+    return {
+      code: '',
+    }
+  }
+})
+</script>
+
 <template>
-  <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
-    <RouterView />
-    <TheFooter />
+  <main
+    flex="~" font-sans p="x-4 y-10" text="center gray-700 dark:gray-200"
+    gap="4"
+  >
+    <Editor v-model="code" />
+    <Editor v-model="result.code" />
   </main>
 </template>
+
+<style lang="scss" scoped>
+
+</style>
