6c50e10925cc8096d5d267ab391a3d3fe54ff684
kkopitehong <kkopite.hong@smartcorelink.com>
2023-09-15 18:18:20 +0800

    feat: 添加shiki高亮输出代码

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,8 +1,12 @@
 <script lang="ts" setup>
 import { processOpenAPI } from '@clscg/core'
+import { getHighlighter } from 'shikiji'
+import raw from './constants/demo.json?raw'
 
-const code = ref('')
+let shiki: any
+const code = ref(raw)
 
+const jsHtml = ref('')
 const result = computed(() => {
   try {
     return processOpenAPI(JSON.parse(code.value))
@@ -13,15 +17,29 @@ const result = computed(() => {
     }
   }
 })
+
+getHighlighter({
+  themes: ['nord'],
+  langs: ['javascript'],
+}).then((highlighter) => {
+  shiki = highlighter
+  jsHtml.value = shiki.codeToHtml(result.value.code, { lang: 'javascript', theme: 'nord' })
+})
+
+watch(() => result.value.code, (code) => {
+  if (shiki) {
+    jsHtml.value = shiki.codeToHtml(code, { lang: 'javascript', theme: 'nord' })
+  }
+})
 </script>
 
 <template>
   <main
-    flex="~" font-sans p="x-4 y-10" text="center gray-700 dark:gray-200"
+    flex="~" font-sans p="x-4 y-10" gray-700 dark:gray-200
     gap="4"
   >
     <Editor v-model="code" />
-    <Editor v-model="result.code" />
+    <div flex="1" class="h-100%" v-html="jsHtml" />
   </main>
 </template>
 
